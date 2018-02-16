58da160b2cec3789126929f8fc4c2909308cfe47
Raju Thapaliya <rlthapaliya@cygnet.com>
2018-02-16 10:41:31 +0530

    LUIS Dialog Unit Conversion
    Added LUIS dialog for Unit Conversion. For Unit Conversion UnitsNet library is used.

---
+++ UnitBot/Helpers/UnitConversionHelper.cs
@@ -0,0 +1,14 @@
+﻿using UnitsNet;
+
+namespace Bot_Application1.Helpers
+{
+    public class UnitConversionHelper
+    {
+        public static string Convert(double amount, string from, string to)
+        {
+            string result = UnitConverter.ConvertByAbbreviation(amount, "Mass", from, to).ToString();
+
+            return result;
+        }
+    }
+}
